58fbdf99ba761d58690e1da10bb1e5ed9a4c1141
Vagan Pogosyan <vaganpogosyan@macbook-pro.myfiosgateway.com>
2021-02-09 12:46:13 -0500

    add alphabet via jQuery

--- hangman-app/app/app.js
+++ hangman-app/app/app.js
@@ -1,3 +1,13 @@
 $(() => {
+    const $lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    for (let letter of $lettersArr) {
+        const $letter = ('<div>' + letter + '</div>');
+        // $($letter).addClass('letter');
+        // console.log($letter.hasClass('class'));
+        $('.letters').append($letter);
+
+    }
+
 
 })
