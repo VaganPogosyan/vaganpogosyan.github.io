ad87024385eafa72cb97dd58119f7ec664766738
Vagan Pogosyan <vaganpogosyan@macbook-pro.myfiosgateway.com>
2021-02-10 13:57:02 -0500

    add underlines for hidden letters of the word

--- hangman-app/app/app.js
+++ hangman-app/app/app.js
@@ -7,9 +7,6 @@ $(() => {
     // create alphabet
     const $lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-
-
-
     for (let letter of $lettersArr) {
 
         const $letter = $('<div>' + letter + '<div>').addClass('letter');
@@ -17,15 +14,32 @@ $(() => {
         console.log($letter.text())
     }
 
-
     // add clicked letters to word div
-
     $('.letter').one('click', (e) => {
         $(e.currentTarget).addClass('letter-selected');
         const $wordLetter = $(e.currentTarget);
         $wordLetter.clone().appendTo($('#word')).removeClass().addClass('word-letter');
-    })
+    });
 
+
+    // add empty border-bottoms for each letter of the hidden word
+    const $wordsArr = ['BRAD PITT'];
+
+
+    for (let element of $wordsArr) {
+        for (let letter of element) {
+            const $hiddenLetter = $('<div>' + letter + '</div>');
+            if (letter === ' ') {
+                $($hiddenLetter).addClass('hidden-space');
+                $('#word').append($hiddenLetter);
+            } else {
+                $($hiddenLetter).addClass('hidden-letter');
+                $('#word').append($hiddenLetter);
+            }
+
+
+        }
+    }
 
 
 
