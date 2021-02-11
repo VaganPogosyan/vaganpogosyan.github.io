1c7d44273cc94b81adc8a6126ed8f0f4802d0bdb
Vagan Pogosyan <vaganpogosyan@MacBook-Pro.local>
2021-02-10 23:24:08 -0500

    set up start and reset buttons

--- hangman-app/app/app.js
+++ hangman-app/app/app.js
@@ -1,44 +1,58 @@
-
-
-
-
-
 $(() => {
-    // create alphabet
+    // ***** create alphabet
     const $lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
     for (let letter of $lettersArr) {
-
         const $letter = $('<div>' + letter + '<div>').addClass('letter');
         $('.letters').append($letter);
         console.log($letter.text())
     }
 
-    // add clicked letters to word div
-    $('.letter').one('click', (e) => {
-        $(e.currentTarget).addClass('letter-selected');
-        const $wordLetter = $(e.currentTarget);
-        $wordLetter.clone().appendTo($('#word')).removeClass().addClass('word-letter');
-    });
+    // function RESET
+    const resetGame = () => {
+        $('.hidden-letter').remove();
+        $('.hidden-space').remove();
+        $('.word-letter').remove();
+        startGame();
+    }
 
+    // function START
+    const startGame = (e) => {
+        $('#start').addClass('start-clicked');
+        // ***** add empty border-bottoms for each letter of the hidden word
+        const $wordsArr = ['BRAD PITT', 'AL PACINO', 'MONICA BELUCCI', 'JENNIFER ANISTON', 'ANNE HATHAWAY', 'DENZEL WASHINGTON', 'MATHEW MCCONAUGHEY', 'JOAQUIN PHOENIX', 'LUPITA NYONGO', 'RYAN REYNOLDS', 'HUGH JACKMAN', 'SCARLET JOHANSSON', 'LEONARDO DICAPRIO', 'CHADWICK BOSEMAN'];
 
-    // add empty border-bottoms for each letter of the hidden word
-    const $wordsArr = ['BRAD PITT', 'AL PACINO', 'MONICA BELUCCI', 'JENNIFER ANISTON', 'ANNE HATHAWAY', 'DENZEL WASHINGTON', 'MATHEW MCCONAUGHEY', 'JOAQUIN PHOENIX', 'LUPITA NYONGO', 'RYAN REYNOLDS', 'HUGH JACKMAN', 'SCARLET JOHANSSON', 'LEONARDO DICAPRIO', 'CHADWICK BOSEMAN',];
+        const $randomIndex = Math.floor(Math.random() * $wordsArr.length);
 
-    const $randomIndex = Math.floor(Math.random() * $wordsArr.length);
+        // ***** add clicked letters to word div
+        $('.letter').one('click', (e) => {
+            $(e.currentTarget).addClass('letter-selected');
+            const $wordLetter = $(e.currentTarget);
+            $wordLetter.clone().appendTo($('#word')).removeClass().addClass('word-letter');
+        });
 
-    // get element from the array
-    const $actor = $wordsArr[$randomIndex];
-    for (i = 0; i < $actor.length; i++) {
-        const $hiddenLetter = $('<div>' + $actor[i] + '</div>');
-        if ($actor[i] === ' ') {
-            $($hiddenLetter).addClass('hidden-space');
-            $('#word').append($hiddenLetter);
-        } else {
-            $($hiddenLetter).addClass('hidden-letter');
-            $('#word').append($hiddenLetter);
-        }
+        if ($('#word') !== '') {
+            // get element from the array
+            const $actor = $wordsArr[$randomIndex];
+            for (i = 0; i < $actor.length; i++) {
+                const $hiddenLetter = $('<div>' + $actor[i] + '</div>');
+                if ($actor[i] === ' ') {
+                    $($hiddenLetter).addClass('hidden-space');
+                    $('#word').append($hiddenLetter);
+                } else {
+                    $($hiddenLetter).addClass('hidden-letter');
+                    $('#word').append($hiddenLetter);
+                };
+            };
+        };
+
+        // buttons RESET 
+        $('#reset').on('click', resetGame);
+
     };
 
+    // buttons START 
+    $('#start').one('click', startGame);
 
-})
+
+});
