f416e8bc78ee1ab2304ff295b55a934b103b7314
Vagan Pogosyan <vaganpogosyan@macbook-pro.myfiosgateway.com>
2021-02-11 19:55:47 -0500

    add logic for winning

--- hangman-app/README.md
+++ hangman-app/README.md
@@ -3,4 +3,4 @@ Refernces:
 
 turn string into an array of strings - https://stackoverflow.com/questions/6484670/how-do-i-split-a-string-into-an-array-of-characters
 
-
+count the amount of divs inside of an element by its id - https://stackoverflow.com/questions/3522002/count-the-number-of-div
--- hangman-app/app/app.js
+++ hangman-app/app/app.js
@@ -7,6 +7,7 @@ $(() => {
         $('.letters').append($letter);
     }
 
+    let $count = 0;
     // function RESET
     const resetGame = () => {
         $('.hidden-letter').remove();
@@ -46,55 +47,67 @@ $(() => {
 
         // remove word from the array so it won't show up as a next hidden word
         $wordsArr.splice($randomIndex, 1);
-        console.log($wordsArr);
 
-        let $count = 0;
+        $count = 0;
+        // console.log($count);
         $('.letter').one('click', (e) => {
             $(e.currentTarget).addClass('letter-selected');
             const $wordLetter = $(e.currentTarget);
             const $actor = $('.hidden-letter');
-
             for (i = 0; i < $actor.length; i++) {
                 if ($wordLetter.text() === $actor.text()[i]) {
                     $($actor[i]).removeClass('hidden-letter').addClass('word-letter');
                 }
             }
 
+            // console.log($('.word-letter').length + 1);
+            // console.log($('#word div').length);
+
+            // if amount of opened letters equals the amount of letters in the hidden word without the space - user wins
+            if ($('#word div').length === $('.word-letter').length + 1) {
+                alert('Good job! To play again hit "Reset".');
+            }
+
             const $hangmanImages = ['style/img/Hangman_1.jpg', 'style/img/Hangman_2.jpg', 'style/img/Hangman_3.jpg', 'style/img/Hangman_4.jpg', 'style/img/Hangman_5.jpg', 'style/img/Hangman_6.jpg', 'style/img/Hangman_loose.jpg'];
 
             if ($actor.text().includes($wordLetter.text()) === false) {
+                // console.log($count);
                 if ($count === 0) {
                     $('#image').attr('src', $hangmanImages[0]);
-                    $count++;
+                    $count = $count + 1;
+                    return;
                 } else if ($count === 1) {
                     $('#image').attr('src', $hangmanImages[1]);
-                    $count++;
+                    $count = $count + 1;
+                    return;
                 } else if ($count === 2) {
                     $('#image').attr('src', $hangmanImages[2]);
-                    $count++;
+                    $count = $count + 1;
+                    return;
                 } else if ($count === 3) {
                     $('#image').attr('src', $hangmanImages[3]);
-                    $count++;
+                    $count = $count + 1;
+                    return;
                 } else if ($count === 4) {
                     $('#image').attr('src', $hangmanImages[4]);
-                    $count++;
+                    $count = $count + 1;
+                    return;
                 } else if ($count === 5) {
                     $('#image').attr('src', $hangmanImages[5]);
-                    $count++;
+                    $count = $count + 1;
+                    return;
                 } else if ($count === 6) {
                     $('#image').attr('src', $hangmanImages[6]);
-                    setTimeout(() => { alert('You Lost') }, 100);
+                    setTimeout(() => { alert('You Lost. Hit the "Reset" button to reset the game.') }, 100);
+                    // console.log('You lost')
                     $('.hidden-letter').removeClass('hidden-letter').addClass('missed-letter');
-                    $count === 0;
+                    // $count = 0;
+                    return;
                 }
             };
 
 
         });
-        // $('#image').attr('src', 'style/img/Hangman_1.jpg');
-
-
-
 
     };
 
